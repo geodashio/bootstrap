d0e9510413c22a9f63624b04310d8faecd613d19
pjdufour <pjdufour.dev@gmail.com>
2016-09-20 11:33:44 +0200

    current work on popup; will be squashed.

--- js/tooltip.js
+++ js/tooltip.js
@@ -375,11 +375,29 @@
   }
 
   Tooltip.prototype.getCalculatedOffset = function (placement, pos, actualWidth, actualHeight) {
-    return placement == 'bottom' ? { top: pos.top + pos.height,   left: pos.left + pos.width / 2 - actualWidth / 2 } :
-           placement == 'top'    ? { top: pos.top - actualHeight, left: pos.left + pos.width / 2 - actualWidth / 2 } :
-           placement == 'left'   ? { top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth } :
-        /* placement == 'right' */ { top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width }
-
+    if(placement == "bottom")
+    {
+      return {
+        top: pos.top + pos.height,
+        left: pos.left + pos.width / 2 - actualWidth / 2
+      };
+    }
+    else if(placement == "top")
+    {
+        return {
+          //top: pos.top - actualHeight,
+          bottom: pos.bottom,
+          left: pos.left + pos.width / 2 - actualWidth / 2
+        };
+    }
+    else if(placement == "left")
+    {
+        return { top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left - actualWidth };
+    }
+    else /* placement == 'right' */
+    {
+        return { top: pos.top + pos.height / 2 - actualHeight / 2, left: pos.left + pos.width };
+    }
   }
 
   Tooltip.prototype.getViewportAdjustedDelta = function (placement, pos, actualWidth, actualHeight) {
